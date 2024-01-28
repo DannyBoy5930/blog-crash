12f30857610ff193b1cad61ef5a9ef1f58195df7
Danny Bao <88077806+DannyBoy5930@users.noreply.github.com>
2024-01-28 17:25:59 +1100

    Implement Tag Categories in Footer component

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,4 @@ export const SITE_TITLE = 'TechPeople Blog';
 export const SITE_DESCRIPTION = 'Articles, stores and tuttorials from Tech People';
 export const HOMEPAGE_ARTICLE_LIMIT = 6;
 export const ARTICLES_PER_PAGE = 6;
+export const FOOTER_TAG_CATERGORIES_LIMIT = 10;
